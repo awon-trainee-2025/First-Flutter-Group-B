78210ec0ae594d3573c362d492228d1660e53680
khalid4440 <khalid142599@gmail.com>
2025-04-15 22:45:15 +0200

    Add files via upload

---
+++ first_page.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'second_page.dart';
+
+class FirstPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            
+            Image.asset(
+              'images/images.jpg',
+              width: 150,
+              height: 150,
+            ),
+
+            SizedBox(height: 30),
+
+            
+            ElevatedButton(
+              child: Text('Go to Second Page'),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ second_page.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Page')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+           
+            Image.asset(
+              'images/images (1).jpg', 
+              width: 150,
+              height: 150,
+            ),
+
+            SizedBox(height: 30),
+
+           
+            ElevatedButton(
+              child: Text('Go Back'),
+              onPressed: () {
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
